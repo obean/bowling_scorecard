66fd2fb68b6897468425cc1949b561f8bda7c9af
ollie beney <ombeney@gmail.com>
2020-10-10 16:51:57 +0100

    frame class

---
+++ src/frame.js
@@ -0,0 +1,25 @@
+class Frame {
+  constructor() {
+    this.strike = false;
+    this.spare = false;
+    this.shots = new Array;
+    this.score = 0;
+  };
+
+  newShot(score) {
+    this.shots.push(score)
+  }
+
+  bonusAttributer() {
+    if(this.shots[0] == 10) { this.strike = true
+    }else if(this.shots.reduce((a,b) => a + b) == 10 && this.shots.length == 2){ this.spare = true}
+  }
+
+  roundGetsBonus() {
+    return (this.strike || this.spare)
+  }
+
+  scoreCalculator() {
+    this.score = this.shots.reduce((a,b) => a + b)
+  }
+}
